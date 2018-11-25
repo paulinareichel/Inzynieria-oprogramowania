fccb7797016f924a962807593117e3a41ad279e8
paulinareichel <43877954+paulinareichel@users.noreply.github.com>
2018-11-25 16:04:24 +0100

    Update Polisa.cpp

--- Polisa.cpp
+++ Polisa.cpp
@@ -4,15 +4,17 @@ int Polisa::numer;
 Polisa::Polisa()
 {
 	numer++;
-	cout << "Wybierz zawartosc polisy: /n 1.Kradziez /n 2.Wypadek/n 3.Porwanie /n 4.Uszczerbek na zdrowiu /n 5.Koniec" << endl;
+	cout << "Wybierz zawartosc polisy: \n 1.Kradziez \n 2.Wypadek \n 3.Porwanie \n 4.Uszczerbek na zdrowiu \n 5.Koniec" << endl;
 	for (int i = 0; i < 4; i++) {
 		int tmp;
 		cin >> tmp;
-		if (tmp != 5)
+		if (tmp == 1 || tmp == 2 || tmp == 3 || tmp == 4)
 			zawartosc[i] = tmp;
-		else break;
-	}
-	cout<< "Podaj date waznosci polisy: "<<endl;
-	cin >> data_waznosci;
-
+		else
+			if (tmp == 5) break;
+			else cout << "Bledne dane" << endl;
+	}	
+	cout << "Podaj date waznosci polisy[RRRR-MM-DD]\n";
+	cout << "Data : ";
+	cin >> data;	
 }
