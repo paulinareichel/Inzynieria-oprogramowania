624be21f7e12841fb965ec0aced9bfecc734932e
paulinareichel <43877954+paulinareichel@users.noreply.github.com>
2018-11-25 11:44:08 +0100

    Update Polisa.cpp

--- Polisa.cpp
+++ Polisa.cpp
@@ -4,7 +4,7 @@ int Polisa::numer;
 Polisa::Polisa()
 {
 	numer++;
-	cout << "Wybierz zawartosc polisy: /n	1.Kradziez	/n 2.Wypadek/n 3.Porwanie /n 4.Uszczerbek na zdrowiu /n 5.Koniec" << endl;
+	cout << "Wybierz zawartosc polisy: /n 1.Kradziez /n 2.Wypadek/n 3.Porwanie /n 4.Uszczerbek na zdrowiu /n 5.Koniec" << endl;
 	for (int i = 0; i < 4; i++) {
 		int tmp;
 		cin >> tmp;
